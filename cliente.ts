e9a330f7771a062c5cdfefc86cae4185c08dc037
Larissa Sevilha <sevilha.larissa10@gmail.com>
2025-04-07 22:51:47 -0300

    install readline

--- cliente.ts
+++ cliente.ts
@@ -1,5 +1,5 @@
 import * as net from "net";
-
+const readline = require("readline-sync");
 function gerarLeitura(banca: number) {
   return {
     banca,
@@ -10,9 +10,44 @@ function gerarLeitura(banca: number) {
 }
 
 const cliente = net.createConnection({ host: "localhost", port: 3000 });
+/*
+setInterval(() => {
+  console.log("ESTADO ATUAL DAS BANCAS:");
+  let temp_bancadas = [];
+  let umidade_bancada = [];
+  let cond_bancada = [];
+  for (const banca in bancas) {
+    var sum = temp_bancadas.reduce((acumulator, currentValue) => {
+      return acumulator + currentValue;
+    }, 0);
+    console.log(
+      `Média: `,
+      sum / 2,
+      banca,
+      "Temperatura: ",
+      (parseFloat(bancas[banca].temperatura) +
+        parseFloat(bancas[banca].temperatura)) /
+        2
+    ),
+      " Umidade: ",
+      parseFloat(bancas[banca].umidade),
+      " Condutividade: ",
+      parseFloat(bancas[banca].condutividade);
 
+    temp_bancadas.push(
+      parseFloat(bancas[banca].temperatura)
+    );
+    umidade_bancada.push( 
+      parseFloat(bancas[banca].umidade)
+    );
+    cond_bancada.push(
+      parseFloat(bancas[banca].condutividade)
+    );
+  }
+}, 5000);
+*/
 cliente.on("connect", () => {
-  for (let banca = 1; banca <= 2; banca++) {
+  for (let banca = 1; banca <= 1; banca++) {
     const leitura = gerarLeitura(banca);
     cliente.write(JSON.stringify(leitura) + "\n");
   }
--- servidor.ts
+++ servidor.ts
@@ -31,38 +31,4 @@ servidor.listen(3000, () => {
   console.log("Servidor escutando na porta 3000");
 });
 
-setInterval(() => {
-  console.log("ESTADO ATUAL DAS BANCAS:");
-  let temp_bancadas = [];
-  let umidade_bancada = [];
-  let cond_bancada = [];
-  for (const banca in bancas) {
-    // console.log(`Banca ${banca}:`, bancas[banca], "Temperatura: ", (parseFloat(bancas[banca].temperatura) + parseFloat(bancas[banca].temperatura)) / 2), " Umidade: ", (parseFloat(bancas[banca].umidade));
-    var sum = temp_bancadas.reduce((acumulator, currentValue) => {
-      return acumulator + currentValue;
-    }, 0);
-    console.log(
-      `Média: `,
-      sum / 2,
-      banca,
-      "Temperatura: ",
-      (parseFloat(bancas[banca].temperatura) +
-        parseFloat(bancas[banca].temperatura)) /
-        2
-    ),
-      " Umidade: ",
-      parseFloat(bancas[banca].umidade),
-      " Condutividade: ",
-      parseFloat(bancas[banca].condutividade);
 
-    temp_bancadas.push(
-      parseFloat(bancas[banca].temperatura)
-    );
-    umidade_bancada.push( 
-      parseFloat(bancas[banca].umidade)
-    );
-    cond_bancada.push(
-      parseFloat(bancas[banca].condutividade)
-    );
-  }
-}, 5000);
